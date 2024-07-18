2367078513e4bba2fd464d1e4a50f16a5db3203d
Sara <sara@Saras-MacBook-Pro-2.local>
2024-07-18 10:41:16 -0400

    return redirect

--- app/actions.ts
+++ app/actions.ts
@@ -162,7 +162,7 @@ export async function likePost(
     const user = await getUser();
 
     if (!user) {
-        redirect("/api/auth/login");
+        return redirect("/api/auth/login");
     }
 
     const postId = formData.get("postId") as string;
